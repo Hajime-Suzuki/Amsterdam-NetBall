72c1fc09f174abfcbc296b66b9d7ea86103a035f
Hajime Suzuki <hajime.doublebass@gmail.com>
2018-09-05 16:22:07 +0200

    activity list

--- client/src/components/admin/ActivityList.js
+++ client/src/components/admin/ActivityList.js
@@ -1,22 +1,58 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Paper from '@material-ui/core/Paper'
+import { Typography, Icon, Checkbox } from '@material-ui/core'
+import styled from 'styled-components'
+import { StyledNameLink } from '../members/MembersListComponent'
 
-const ActivityList = ({ activities }) => {
+const ActivityCard = styled(Paper)`
+  margin: 2em 0;
+`
+
+const formatTiem = time => {
+  return time.replace(/(\d{4})-(\d{2})-(\d{2})T(\d{2}:\d{2}).*/, '$4 $3/$2')
+}
+
+const ActivityList = ({ activities, attendances, changeAttendance }) => {
   if (!activities.length) return null
+
   return activities.map(act => {
-    const members = act.members.map(m => {
-      return (
-        <p key={m.id}>
-          {m.firstName} {m.lastName}
-        </p>
-      )
-    })
+    // console.log('===', act)
+    const members = act.members ? (
+      act.members.map(m => {
+        const thisAttendance = Object.values(attendances).find(
+          att => att.memberId === m.id && att.activityId === act.id
+        )
+        return (
+          <div key={m.id}>
+            <Checkbox
+              checked={thisAttendance.isAttended}
+              color="primary"
+              onChange={() => changeAttendance(thisAttendance.id)}
+            />
+            <StyledNameLink to={`/members/${m.id}`}>
+              {m.firstName} {m.lastName}
+            </StyledNameLink>
+          </div>
+        )
+      })
+    ) : (
+      <Typography>No member</Typography>
+    )
 
     return (
-      <div key={act.id}>
-        <p>{act.name}</p>
-        <p>{members}</p>
-        <p>===</p>
-      </div>
+      <ActivityCard key={act.id}>
+        <Typography variant="display1">{act.name}</Typography>
+        <Typography>
+          <Icon className="fas fa-clock" />
+          {formatTiem(act.startTime)} - {formatTiem(act.endTime)}
+        </Typography>
+        <Typography>
+          <Icon className="fas fa-map-marker-alt" />
+          {act.location}
+        </Typography>
+        {members}
+      </ActivityCard>
     )
   })
 }
--- client/src/components/admin/ActivityListComponent.js
+++ client/src/components/admin/ActivityListComponent.js
@@ -3,22 +3,37 @@ import ActivityList from './ActivityList'
 import { connect } from 'react-redux'
 import {
   getActivitiesAndMembers,
-  activitiesArraySelector
+  activitiesArraySelector,
+  editAttendance
 } from '../../redux/actions/activities'
 
 class AcivityListComponent extends Component {
   componentDidMount() {
     this.props.getActivitiesAndMembers()
   }
+  changeAttendance = attendanceId => {
+    // console.log(attendanceId)
+    this.props.editAttendance(attendanceId)
+  }
   render() {
-    return <ActivityList activities={this.props.activities} />
+    return (
+      <ActivityList
+        activities={this.props.activities}
+        attendances={this.props.attendances}
+        changeAttendance={this.changeAttendance}
+      />
+    )
   }
 }
 
 const mapSateToProps = state => ({
-  activities: activitiesArraySelector(state)
+  activities: activitiesArraySelector(state),
+  attendances: (state.activities && state.activities.attendance) || {}
 })
 export default connect(
   mapSateToProps,
-  { getActivitiesAndMembers }
+  {
+    getActivitiesAndMembers,
+    editAttendance
+  }
 )(AcivityListComponent)
